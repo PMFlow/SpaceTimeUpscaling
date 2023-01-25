f986d88188444fb50aeacdcf91a1b5b335491943
PMFlow <48481148+PMFlow@users.noreply.github.com>
2023-01-25 12:46:44 +0100

    Delete CGST_Soils directory

--- CGST_Soils/theta_GM_flow.m
+++
@@ -1,8 +0,0 @@
-function [tht]=theta_GM_flow(theta_res,theta_sat,p,alpha,nGM,ng)
-           
-theta_neg = theta_res+(theta_sat-theta_res)*(1./(1+(-alpha*p).^nGM)).^ng;
-theta_pos = theta_sat;
-neg = p < 0; 
-tht = theta_neg.*neg + theta_pos.*~neg;
-
- 
